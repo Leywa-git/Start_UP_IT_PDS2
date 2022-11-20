e7d894bb3b9d56b53491aca07b3d250fd55727da
Oleksii Lysykhin <oleksilysykhin@gmail.com>
2022-11-20 15:58:05 +0200

    dz_11_1 ready

---
+++ Lesson_11/dz_11_1_client.py
@@ -0,0 +1,12 @@
+import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect(("localhost", 60000))
+
+while True:
+    message = input("Enter the message:")
+    print("Awaiting for a reply")
+    sock.send(message.encode())
+    message = (sock.recv(1024)).decode()
+    print("Server:", message)
+sock.close()
---
+++ Lesson_11/dz_11_1_server.py
@@ -0,0 +1,18 @@
+import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind(("", 60000))
+sock.listen(10)
+print("Server is running, press CTRL+C to stop")
+
+conn, addr = sock.accept()
+print("Received connection from:", addr)
+
+while True:
+    print("Awaiting for the client inquiry")
+    message = conn.recv(1024)
+    message = message.decode()
+    print("Client:", message)
+    message = input("Enter the answer: ")
+    conn.send(message.encode())
+conn.close()
