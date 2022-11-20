6d8045cd2f0ff71944143b99e6c0bbcbc555bb6a
Oleksii Lysykhin <oleksilysykhin@gmail.com>
2022-11-20 16:36:44 +0200

    dz_11_3 ready

---
+++ Lesson_11/dz_11_3_server.py
@@ -0,0 +1,24 @@
+import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind(("", 60000))
+sock.listen(10)
+print("Server is running, press CTRL+C to stop")
+
+conn, addr = sock.accept()
+print("Received connection from:", addr)
+
+while True:
+    message = conn.recv(1024)
+    message = message.decode()
+    print("Client:", message)
+    data = message.split()
+    print(data)
+    counter = 0
+    for i in data:
+        if not i.isdigit():
+            counter += 1
+    message = f"Number of words used by Client: {counter}"
+    print(counter, "words used by Client")
+    conn.send(message.encode())
+conn.close()
