2400df2a005ed7c652aafd10edd8edb71dbdff7a
Oleksii Lysykhin <oleksilysykhin@gmail.com>
2022-11-23 15:21:13 +0200

    dz_12_2 half-finished

--- Lesson_12/dz_12_2_server.py
+++ Lesson_12/dz_12_2_server.py
@@ -30,18 +30,30 @@ async def server():
     conn, addr = sock.accept()
     print("Received connection from:", addr)
 
-    greeting = "Hello! You are connected to Server."
+    greeting = "Hello! You are connected to Server.\n" \
+               "Enter q in order to disconnect from Server.\n" \
+               "Enter 2 numbers to receive equations with them."
+
     conn.send(greeting.encode())
 
-    while True:
-        print("Awaiting for the client inquiry")
+    message = conn.recv(1024)
+    message = message.decode()
+    print("Client:", message)
+
+    while message != "quit":
         message = conn.recv(1024)
         message = message.decode()
         print("Client:", message)
         data = message.split()
-        addition().send(data)
-        subtraction().send(data)
-        multiply().send(data)
+        loop = asyncio.get_event_loop()
+        equation_1 = loop.create_task(addition())
+        equation_2 = loop.create_task(subtraction())
+        equation_3 = loop.create_task(multiply())
+        equations_await = asyncio.wait(equation_1)
+        loop.run_until_complete()
+        result = f"{equation_1} \n" \
+                 f"{equation_2} \n" \
+                 f"{equation_3}"
         conn.send(message.encode())
     conn.close()
 
