817b011a28cb2f0c3fca971bdc0ff79f984d50bf
Oleksii Lysykhin <oleksilysykhin@gmail.com>
2022-11-23 14:45:04 +0200

    dz_12_2 half-finished

--- Lesson_12/dz_12_2_server.py
+++ Lesson_12/dz_12_2_server.py
@@ -1,17 +1,6 @@
 import asyncio
 import socket
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind(("", 60000))
-sock.listen(10)
-print("Server is running, press CTRL+C to stop")
-
-conn, addr = sock.accept()
-print("Received connection from:", addr)
-
-greeting = "Hello! You are connected to Server."
-conn.send(greeting.encode())
-
 
 async def addition():
     first_number, second_number = [data[i] for i in (0, 1)]
@@ -31,14 +20,30 @@ async def multiply():
     return message
 
 
-while True:
-    print("Awaiting for the client inquiry")
-    message = conn.recv(1024)
-    message = message.decode()
-    print("Client:", message)
-    data = message.split()
-    addition().send(data)
-    subtraction().send(data)
-    multiply().send(data)
-    conn.send(message.encode())
-conn.close()
+async def server():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind(("", 60000))
+    sock.listen(10)
+    sock.setblocking(False)
+    print("Server is running, press CTRL+C to stop")
+
+    conn, addr = sock.accept()
+    print("Received connection from:", addr)
+
+    greeting = "Hello! You are connected to Server."
+    conn.send(greeting.encode())
+
+    while True:
+        print("Awaiting for the client inquiry")
+        message = conn.recv(1024)
+        message = message.decode()
+        print("Client:", message)
+        data = message.split()
+        addition().send(data)
+        subtraction().send(data)
+        multiply().send(data)
+        conn.send(message.encode())
+    conn.close()
+
+
+asyncio.run(server())
