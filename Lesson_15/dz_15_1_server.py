eb5ffce3f2a3bdbeb8783048ab3b2bafa6935ba3
Oleksii Lysykhin <oleksilysykhin@gmail.com>
2023-02-05 00:19:11 +0200

    dz_15_1 half-finished

--- Lesson_15/dz_15_1_server.py
+++ Lesson_15/dz_15_1_server.py
@@ -1,30 +1,44 @@
 import socket
 import logging
-import dz
 
-module_logger = logging.getLogger("Server_Log")
 
-fh = logging.FileHandler("Server_Log")
+def main():
+    logger = logging.getLogger("dz_15_1_logger.conf")
+    logger.setLevel(logging.DEBUG)
 
-try:
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.bind(("", 60000))
-    sock.listen(10)
-    print("Server is running, press CTRL+C to stop")
+    fh = logging.FileHandler("dz_15_1.log")
 
-    conn, addr = sock.accept()
-    print("Received connection from:", addr)
+    formatter = logging.Formatter('server %(asctime)s - %(levelname)s - %(message)s')
+    fh.setFormatter(formatter)
 
-    greeting = "Hello! You are connected to Server."
-    conn.send(greeting.encode())
-except Exception:
-    logging.error("Server does not respond")
+    logger.addHandler(fh)
 
-while True:
-    print("Awaiting for the client inquiry")
-    message = conn.recv(1024)
-    message = message.decode()
-    print("Client:", message)
-    message = input("Enter the answer: ")
-    conn.send(message.encode())
-conn.close()
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.bind(("", 60000))
+        sock.listen(10)
+        print("Server is running, press CTRL+C to stop")
+
+        conn, addr = sock.accept()
+        print("Received connection from:", addr)
+
+        greeting = "Hello! You are connected to Server."
+        conn.send(greeting.encode())
+        logging.info("Server is up")
+    except Exception:
+        logging.error("Server does not respond")
+
+    while True:
+        print("Awaiting for the client inquiry")
+        message = conn.recv(1024)
+        message = message.decode()
+        print("Client:", message)
+        if message == "get lost":
+            logging.warning("avoid client in the future")
+        message = input("Enter the answer: ")
+        conn.send(message.encode())
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
