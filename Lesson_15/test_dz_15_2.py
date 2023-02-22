b2e5249615d9c1a270cc0706bf5311dcf856bf2a
Oleksii Lysykhin <oleksilysykhin@gmail.com>
2023-02-22 14:37:09 +0200

    dz_15_2 finished (results in test_dz_15_2)

--- Lesson_15/test_dz_15_2.py
+++ Lesson_15/test_dz_15_2.py
@@ -2,22 +2,26 @@ import pytest
 from dz_15_2 import Tree
 from dz_15_2 import nodes, root_node
 
+n = nodes
+r = root_node
+
 
 def test_append_new_nodes():
-    root_node.append_new_nodes(nodes)
-    assert root_node.min_node() == 1
+    r.append_new_nodes(n)
+    assert r.min_node() == 1
+    assert r.max_node() == 15
 
 
 def test_min_node():
-    root_node.insert(0)
-    assert root_node.min_node() != 1
+    r.insert(0)
+    assert r.min_node() != 1
 
 
 def test_max_node():
-    root_node.insert(25)
-    assert root_node.max_node() == 25
+    r.insert(25)
+    assert r.max_node() == 25
 
 
 def test_node_delete():
-    root_node.node_delete(root_node, 25)
-    assert root_node.max_node() != 25
+    r.node_delete(r, 25)
+    assert r.max_node() != 25
